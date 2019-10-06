9101b2721bccf6f9e5713c90599fe25ff9d5c639
Grégory Latinier <gregory.latinier@gmail.com>
2019-10-06 10:03:03 +0000

    Project init

---
+++ src/layouts/main.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          icon="menu"
+          aria-label="Menu"
+        />
+
+        <q-toolbar-title>
+          Telos Net
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
---
+++ src/pages/404.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="fixed-center text-center">
+    <p class="text-faded">Sorry, nothing here...<strong>(404)</strong></p>
+    <q-btn
+      color="secondary"
+      to="/"
+      label="Go back"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'page-404'
+}
+</script>
