41cfa24c6744917e261913b4acf5981e9527d469
DonaldPeat <don@caleos.io>
2021-10-27 21:45:46 -0500

    fix: icon & column alignment

--- src/pages/validators/components/ValidatorDataTable.vue
+++ src/pages/validators/components/ValidatorDataTable.vue
@@ -8,6 +8,62 @@
       row-key="__index"
       @row-click="rowClicked"
     )
+      q-tr( slot="body" slot-scope="props" :props="props")
+        q-td( key="number") {{props.cols[0].value}}
+        q-td( key="owner" ) {{props.cols[1].value }}
+        q-td( key="votes" align="center") {{props.cols[2].value }}
+        q-td( key="sslVerified" align='left')
+          q-icon(
+            v-if="props.cols[3].value === true"
+            name="fas fa-check"
+            size="xs"
+            color='green'
+          )
+          q-icon(
+            v-else
+            name="fas fa-times"
+            size="xs"
+            color='red'
+          )
+        q-td( key="apiVerified" align='left')
+          q-icon(
+            v-if="props.cols[4].value === true"
+            name="fas fa-check"
+            size="xs"
+            color='green'
+          )
+          q-icon(
+            v-else
+            name="fas fa-times"
+            size="xs"
+            color='red'
+          )
+        q-td( key="sslVerifiedTestNet" align='left')
+          q-icon(
+            v-if="props.cols[5].value === true"
+            name="fas fa-check"
+            size="xs"
+            color='green'
+          )
+          q-icon(
+            v-else
+            name="fas fa-times"
+            size="xs"
+            color='red'
+          )
+        q-td( key="apiVerifiedTestNet" align='left')
+          q-icon(
+            v-if="props.cols[6].value === true"
+            name="fas fa-check"
+            size="xs"
+            color='green'
+          )
+          q-icon(
+            v-else
+            name="fas fa-times"
+            size="xs"
+            color='red'
+          )
 </template>
 
 <script>
@@ -29,43 +85,49 @@ export default {
           name: 'number',
           label: '#',
           field: row => this.producerData.indexOf(row) + 1,
+          align: 'left',
           sortable: true
         },
         {
           name: 'owner',
           label: 'Block Producer',
           field: 'owner',
+          align: 'left',
           sortable: true
         },
         {
           name: 'votes',
           label: 'Total Votes',
           field: row => (row.total_votes / 1000000000).toFixed(1) + 'B',
+          align: 'center',
           sortable: true
         },
         {
           name: 'sslVerified',
           label: 'SSL',
           field: row => row.sslVerified === true,
+          align: 'left',
           sortable: true
         },
         {
           name: 'apiVerified',
           label: 'API',
           field: row => row.apiVerified === true,
+          align: 'left',
           sortable: true
-
         },
         {
           name: 'sslVerifiedTestNet',
-          label: 'SSL(test net)',
+          label: 'SSL*',
           field: row => row.sslVerifiedTestNet === true,
+          align: 'left',
           sortable: true
         },
         {
           name: 'apiVerifiedTestNet',
-          label: 'API(test net)',
+          label: 'API*',
           field: row => row.apiVerifiedTestNet === true,
+          align: 'left',
           sortable: true
         }
       ]
